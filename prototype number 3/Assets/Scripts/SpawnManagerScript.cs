653761ffdb0a117e2963dac5dd447e31dbc81b01
Ty1er47 <111007514+Ty1er47@users.noreply.github.com>
2022-10-12 11:02:36 -0700

    animation

--- prototype number 3/Assets/Scripts/SpawnManagerScript.cs
+++ prototype number 3/Assets/Scripts/SpawnManagerScript.cs
@@ -4,10 +4,14 @@ using UnityEngine;
 
 public class SpawnManagerScript : MonoBehaviour
 {
+    public GameObject obstaclePreFab;
+    private Vector3 _spawnPos = new Vector3(12, -3.25f, 0);
+    private float _startDelay = 2;
+    private float _repeatRate = 3;
     // Start is called before the first frame update
     void Start()
     {
-        
+        InvokeRepeating("SpawnObstacle", _startDelay, _repeatRate);
     }
 
     // Update is called once per frame
